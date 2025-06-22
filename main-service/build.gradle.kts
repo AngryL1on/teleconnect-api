b6935bdecfe5eda57cc97ac9328c67906b16e8f1
AngryL1on <vadim.angryl1on@gmail.com>
2025-06-22 23:24:07 +0300

    Add OpenApiConfig, migrate .properties to .yml configs

--- main-service/build.gradle.kts
+++ main-service/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("com.h2database:h2")
